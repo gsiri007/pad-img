c479f9bdc396bb2a7c37853e2527f5cd224c15f9
gsiri007 <216904534+gsiri007@users.noreply.github.com>
2025-09-11 20:12:11 +0530

    clean up - removing function

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import argparse
 from PIL import Image, ImageOps
 
 def main():
-
     # command line argument parsing
     HELP_MESSAGE = '--- USAGE DETAILS ---'
 
@@ -12,28 +11,21 @@ def main():
     parser.add_argument('image')
 
     # optional argumentsg
-    parser.add_argument('-o', '--output', type=str, default=None)
+    parser.add_argument('-o', '--output', type=str, default='output')
     parser.add_argument('-b', '--background', type=str, default='black')
 
     args = parser.parse_args()
 
     INPUT_IMAGE = args.image
     OUTPUT_IMAGE = args.output
-    IMAGE_BACKGROUND = args.background
+    BACKGROUND_COLOR = args.background
 
-    image_to_wallpaper(INPUT_IMAGE, IMAGE_BACKGROUND)
-
-def image_to_wallpaper(image: str, bg_color: str):
 
     try:
-        img = Image.open(image)
-        scaled_width = img.size[0]
-        scaled_height = 3000
-        scaled_size = (scaled_width, scaled_height)
-        scaled_img = img.resize(scaled_size, Image.Resampling.LANCZOS)
+        img = Image.open(INPUT_IMAGE)
 
-        pad_size = (6000, scaled_height)
-        final_image = ImageOps.pad(scaled_img, pad_size, color=bg_color)
+        pad_size = (6000, 3000)
+        final_image = ImageOps.pad(img, pad_size, color=BACKGROUND_COLOR)
 
         final_image.show()
 
