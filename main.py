3d6ab48944a45f3e97bd37b4803e2318e422cefc
gsiri007 <216904534+gsiri007@users.noreply.github.com>
2025-09-11 20:26:43 +0530

    save the file with a specific format

--- main.py
+++ main.py
@@ -13,21 +13,26 @@ def main():
     # optional argumentsg
     parser.add_argument('-o', '--output', type=str, default='output')
     parser.add_argument('-b', '--background', type=str, default='black')
+    parser.add_argument('-f', '--format', type=str, default=None)
 
     args = parser.parse_args()
 
     INPUT_IMAGE = args.image
     OUTPUT_IMAGE = args.output
     BACKGROUND_COLOR = args.background
+    OUTPUT_FORMAT = args.format
 
-
+    # creating the wallpaper
     try:
         img = Image.open(INPUT_IMAGE)
 
         pad_size = (6000, 3000)
         final_image = ImageOps.pad(img, pad_size, color=BACKGROUND_COLOR)
 
-        final_image.show()
+        if OUTPUT_FORMAT is None:
+            final_image.save(f"{OUTPUT_IMAGE}.{str(img.format).lower()}", img.format)
+        else:
+            final_image.save(f"{OUTPUT_IMAGE}.{OUTPUT_FORMAT.lower()}", OUTPUT_FORMAT)
 
     except OSError as e:
         print(str(e))
